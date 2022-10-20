c3b612c8e8d83853d0de3722769bd1f5cf715594
andre <andrelbarcelo@gmail.com>
2022-10-20 11:39:28 -0300

    atualizacoes-front

--- templates/static/js/login.js
+++ templates/static/js/login.js
@@ -1,1 +1,19 @@
-alert("Oier");
+$(() => {
+
+    $("input").on("focus", function(event) {
+      const div = $(this).parent(".input-field");
+      const label = div.children("label");
+      
+      label.css("top", "-10px");
+    });
+    
+    $("input").on("blur", function(event) {
+      const div = $(this).parent(".input-field");
+      const label = div.children("label");
+      
+      if ($(this).val().length == 0) {
+        label.css("top", "12px");
+      }
+    });
+  
+  });
